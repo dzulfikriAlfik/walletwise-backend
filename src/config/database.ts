f9e0b35bd58c70cb4b2316101f2f5814f5db7df8
Dzulfikri Alkautsari <dev.dzulfikri.alkautsari@gmail.com>
2026-04-29 22:14:05 +0700

    refactor: hide parsed password from logging at the server start

--- src/config/database.ts
+++ src/config/database.ts
@@ -34,7 +34,7 @@ function sanitizeDbUrl(url: string): string {
   try {
     const parsed = new URL(url)
     if (parsed.password) {
-      // parsed.password = '****'
+      parsed.password = '****'
     }
     return parsed.toString()
   } catch {
